496b92215226ea04f3b7ce3b42ec2d388e6203ed
Sundeep Kakar <skakar@iu.edu>
2019-05-23 18:46:02 +0530

    Updates

---
+++ precache-manifest.5797ae594b2ee6b4e1898b044e29cc3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201e8ac287564928eaffd28aca0ad1ed",
+    "url": "/coder-reference-guide/index.html"
+  },
+  {
+    "revision": "6683a94b0f5d15229c03",
+    "url": "/coder-reference-guide/static/css/main.7e6ba4a8.chunk.css"
+  },
+  {
+    "revision": "ee7e1e29b4b07e6e046b",
+    "url": "/coder-reference-guide/static/js/2.a2615ae8.chunk.js"
+  },
+  {
+    "revision": "6683a94b0f5d15229c03",
+    "url": "/coder-reference-guide/static/js/main.48aff857.chunk.js"
+  },
+  {
+    "revision": "c23d44b121268444a9ff",
+    "url": "/coder-reference-guide/static/js/runtime~main.27517e7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coder-reference-guide/static/media/logo.5d5d9eef.svg"
+  }
+]);
